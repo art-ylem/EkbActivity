51fd01f44a70a06db1f697fe397e6bafced1c6a6
Art-ylem <art-ylem@mail.ru>
2019-03-29 23:41:08 +0500

    map

--- app/src/main/java/com/example/ekbactivity/MapFragment.java
+++ app/src/main/java/com/example/ekbactivity/MapFragment.java
@@ -11,8 +11,8 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -56,8 +56,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
+        MapView mapFragment = view.findViewById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
